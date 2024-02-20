6aad05436e35e65abcb74a30a0d8ad6f795e8c5c
Temurbek <temurbekunical@gmail.com>
2024-02-20 17:19:38 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+Your Test budget app
+Overview
+Brief description of your React app.
+
+Prerequisites
+Make sure you have Node.js and npm installed on your machine.
+
+Node.js: Download Here
+npm: Installation Guide
+Getting Started
+
+1. Clone the Repository
+   bash
+   Copy code
+   git clone https://github.com/your-username/your-react-app.git
+   cd your-react-app
+2. Install Dependencies
+   bash
+   Copy code
+   yarn install
+   Development
+3. Run the Development Server
+   bash
+   Copy code
+   yarn run dev
+   This will start the development server. Open http://localhost:3000 in your browser to view your app.
+
+Testing 4. Run Tests
+bash
+Copy code
+yarn run test
+Run your unit tests using your testing framework of choice.
+
+Building for Production 5. Build for Production
+bash
+Copy code
+yarn run build
+This command will generate a production-ready build in the dist folder.
+
+6. Serve the Production Build Locally
+   bash
+   Copy code
+   yarn run serve
+   This will serve the production build locally, allowing you to preview the app as it would appear in a production environment.
+
+Additional Commands
+yarn run lint: Lint your code.
+yarn run format: Format your code.
+Contributing
+If you'd like to contribute, please follow the Contribution Guidelines.
+
+License
+This project is licensed under the MIT License.
---
+++ src/components/BudgetItem.jsx
@@ -0,0 +1,76 @@
+// rrd imports
+import { Form, Link } from "react-router-dom";
+
+// library imports
+import {
+  BanknotesIcon,
+  TrashIcon,
+  MinusCircleIcon,
+} from "@heroicons/react/24/outline";
+
+// helper functions
+import {
+  calculateSpentByBudget,
+  formatCurrency,
+  formatPercentage,
+} from "../helpers";
+
+const BudgetItem = ({ budget, showDelete = false }) => {
+  const { id, name, amount, color, description } = budget;
+  const spent = calculateSpentByBudget(id);
+
+  return (
+    <div
+      className="budget"
+      style={{
+        "--accent": color,
+      }}
+    >
+      <div className="progress-text">
+        <h3>{name}</h3>
+        <div className="tooltip"></div>
+        <p>{formatCurrency(amount)} Budgeted </p>
+      </div>
+      <progress max={amount} value={spent}>
+        {formatPercentage(spent / amount)}
+      </progress>
+      <div className="progress-text">
+        <small>{formatCurrency(spent)} spent</small>
+        <small className="flex-sm align-items-center">
+          {formatCurrency(amount - spent)} remaining
+          {amount - spent < 0 ? <MinusCircleIcon width={30} /> : ""}
+        </small>
+      </div>
+      {showDelete ? (
+        <div className="flex-sm">
+          <Form
+            method="post"
+            action="delete"
+            onSubmit={(event) => {
+              if (
+                !confirm(
+                  "Are you sure you want to permanently delete this budget?"
+                )
+              ) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit" className="btn">
+              <span>Delete Budget</span>
+              <TrashIcon width={20} />
+            </button>
+          </Form>
+        </div>
+      ) : (
+        <div className="flex-sm">
+          <Link to={`/budget/${id}`} className="btn">
+            <span>View Details</span>
+            <BanknotesIcon width={20} />
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+export default BudgetItem;
